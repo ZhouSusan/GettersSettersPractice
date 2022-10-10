ecbcec46f1d1855eaca1d2a170cfeb1aec8d03be
ZhouSusan <szhou089@gmail.com>
2022-10-09 23:56:27 -0700

    set Donkey Kong's credit card number in main.Java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         CreditCard cc = new CreditCard();
         cc.setName("Donkey Kong");
+        cc.setCreditCardNumber("1435671748598592");
     }
 }
