b3df1bfd55f4264b1dd05ee8453a40800ff02a8d
ZhouSusan <szhou089@gmail.com>
2022-10-09 23:59:30 -0700

    created a print out of the card details using getters

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,10 @@ public class Main {
         cc.setCreditCardNumber("1435671748598592");
         cc.setExpirationDate("10/28");
         cc.setCvv(555);
+
+        System.out.println("Your card details:\nCardholder Name: " + cc.getName());
+        System.out.println("Card Number: " + cc.getCreditCardNumber());
+        System.out.println("Expiration Date: " + cc.getExpirationDate());
+        System.out.println("CVV: " + cc.getCvv());
     }
 }
