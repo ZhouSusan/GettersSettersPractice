7a7b7de8090d9324cb82263900b00025b2234ea0
ZhouSusan <szhou089@gmail.com>
2022-10-09 23:48:53 -0700

    created getters and setters for name in CreditCard.java

--- src/main/java/org/example/CreditCard.java
+++ src/main/java/org/example/CreditCard.java
@@ -5,4 +5,14 @@ public class CreditCard {
     private String creditCardNumber;
     private String expirationDate;
     private int cvv;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    
 }
