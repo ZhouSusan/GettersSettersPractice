dfd4c980c56a586a6b9f469610ec273af44e0165
ZhouSusan <szhou089@gmail.com>
2022-10-09 23:57:30 -0700

    set Donkey Kong's credit card expiration date and cvv in main.Java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,7 @@ public class Main {
         CreditCard cc = new CreditCard();
         cc.setName("Donkey Kong");
         cc.setCreditCardNumber("1435671748598592");
+        cc.setExpirationDate("10/28");
+        cc.setCvv(555);
     }
 }
